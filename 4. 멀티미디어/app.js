38ab9688e4548329653507467a9f8b06e85c3f3c
soohouse <misowltn@gmail.com>
2022-06-19 23:40:31 +0900

    220619

---
+++ 4. 멀티미디어/app.js
@@ -0,0 +1,8 @@
+
+
+const $btn = document.querySelector('#change');
+const $pet = document.querySelector('#my-pet');
+
+$btn.addEventListener('click', e => {
+    $pet.textContent = '못난이 거북이!!';
+});
